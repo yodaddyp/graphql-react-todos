228dc40ee0fcb681d350e2875eb7606a27dda73e
Paul <me@paulbill.com>
2020-02-04 16:49:46 +0000

    Add Todo list

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Typography  from '@material-ui/core/Typography';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => (
+    <>
+        <Typography variant="h1" component="h1">
+            Todos
+        </Typography>
+        <Todos />
 
-export default App;
+    </>
+);
---
+++ app/src/components/Todo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+
+export default function Todo({ completed, due, id, text }) {
+    const [checked, setChecked] = React.useState(!!completed);
+
+    const labelId = `label-${id}`;
+    const primary = text;
+    const secondary = checked
+      ? `Completed on ${completed}`
+      : `Due on ${due}`;
+
+    return (
+        <ListItem role={undefined} dense button onClick={() => setChecked(!checked)}>
+            <ListItemIcon>
+                <Checkbox
+                    checked={checked}
+                    edge="start"
+                    inputProps={{ 'aria-labelledby': labelId }}
+                    tabIndex={-1}
+                />
+            </ListItemIcon>
+            <ListItemText
+                id={labelId}
+                primary={primary}
+                secondary={secondary}
+            />
+        </ListItem>
+    )
+}
