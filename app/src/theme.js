9ac8988ee858b297700bc8001d2d319869fe8136
Paul <me@paulbill.com>
2020-02-04 16:49:11 +0000

    Add material ui theme and remove CRA default styling

---
+++ app/src/theme.js
@@ -0,0 +1,8 @@
+import { createMuiTheme } from "@material-ui/core";
+import { blue } from '@material-ui/core/colors';
+
+export default createMuiTheme({
+    palette: {
+        primary: blue
+    }
+})
