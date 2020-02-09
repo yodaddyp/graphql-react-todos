2fda05da381a45eda076ba3c3c71ccdb810efdfc
Paul <me@paulbill.com>
2020-02-09 18:51:31 +0000

    Add a subscription to the todos table and pull down the latest todos

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,35 +1,57 @@
 import React from 'react';
-import ApolloClient, { gql } from 'apollo-boost';
+
+import ApolloClient from 'apollo-client';
+import { WebSocketLink } from 'apollo-link-ws';
+import { HttpLink } from 'apollo-link-http';
+import { split } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from '@apollo/react-hooks';
+
 import Typography  from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Paper from '@material-ui/core/Paper';
 import Todos from './containers/Todos';
 import NewTodo from './containers/NewTodo';
 
+const API_ENDPOINT = `http://localhost:8080/v1/graphql`;
+
 const headers = {
     'content-type': 'application/json',
     'x-hasura-admin-secret': 'mysecret',
 };
 
-const API_ENDPOINT = `http://localhost:8080/v1/graphql`;
-
-const client = new ApolloClient({
+const httpLink = new HttpLink({
     headers,
     uri: API_ENDPOINT
 });
 
-client.query({
-    query: gql`query MyQuery {
-  todos {
-    id
-    text
-    due
-    completed
-  }
-}
-`
-}).then(res => console.log(res));
+const webSocketLink = new WebSocketLink({
+    options: {
+        lazy: true,
+        reconnect: true,
+        connectionParams: () => ({ headers })
+    },
+    uri: "ws://localhost:8080/v1/graphql"
+});
+
+// using the ability to split links, you can send data to each link
+// depending on what kind of operation is being sent
+const link = split(
+    // split based on operation type
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        console.log(kind, operation);
+        return kind === 'OperationDefinition' && operation === 'subscription';
+    },
+    webSocketLink,
+    httpLink,
+);
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link
+});
 
 export default () => (
     <ApolloProvider client={client}>
--- app/src/containers/Todos.jsx
+++ app/src/containers/Todos.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { Subscription } from '@apollo/react-components';
 import List from '@material-ui/core/List';
 import Todo from './../components/Todo';
+
+const FETCH_TODOS = gql`
+subscription fetchTodos {
+  todos(order_by: {createdAt: desc, completed: asc_nulls_first}) {
+    id
+    text
+    due
+    completed
+  }
+}
+`;
 
 export default function Todos() {
     return (
         <List>
-            <Todo id="123" completed="31st Jan" due="1st Feb" text="To your homework" />
+            <Subscription subscription={FETCH_TODOS}>
+                {({ data, loading, error }) => {
+                    if (loading) return null; // Would be nicer to add some loading feedback here.
+
+                    if (error) return <p>There has been an error making a subscription</p>;
+
+                    const { todos } = data;
+                    return todos.map(todo => (
+                        <Todo {...todo} />
+                    ))
+                }}
+            </Subscription>
         </List>
     )
 }
